3383485b4dc739d80010ff8ea39e98e4693a5953
Akshika Garg <105767216+Akshikagarg@users.noreply.github.com>
2023-10-03 21:56:38 +0530

    new changes

--- src/modules/timetableModule/controllers/semtable.js
+++ src/modules/timetableModule/controllers/semtable.js
@@ -3,36 +3,18 @@ const TimetableEntry = require('../../../models/student');
 class StudentController{
     createTimetableEntry = async (req, res) => {
         try {
-          const {
-            Slot,
-            SubCode,
-            Day,
-            Type,
-            SubName,
-            Sem,
-            Branch,
-            Designation,
-          } = req.body;
+          const newentry = req.body;
       
       
           // Check if a timetable entry already exists for the given Slot and Day
-          const existingEntry = await TimetableEntry.findOne({ Slot, Day });
+          const existingEntry = await TimetableEntry.findOne({ Slot: newentry.Slot, Day: newentry.Day });
       
           if (existingEntry) {
             return res.status(400).json({ error: 'Timetable slot already occupied' });
           }
       
           // Create a new timetable entry
-          const newTimetableEntry = new TimetableEntry({
-            Slot,
-            SubCode,
-            Day,
-            Type,
-            SubName,
-            Sem,
-            Branch,
-            Designation,
-          });
+          const newTimetableEntry = req.body;
       
           const createdEntry = await TimetableEntry.create(newTimetableEntry);
           res.json(newTimetableEntry)
