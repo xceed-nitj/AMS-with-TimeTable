f888da44c99782c239bd1d0860d96b3e7dd2f7b5
harimurugan1989 <harimur@gmail.com>
2023-10-11 12:22:32 +0530

    folder structure corrected

---
+++ client/src/timetableadmin/facultydata.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+
+function FacultyTable() {
+  const [facultyData, setFacultyData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Make a GET request to fetch faculty data when the component mounts
+    fetch('http://127.0.0.1:8000/timetablemodule/faculty')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFacultyData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching faculty data:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Faculty Timetable</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Designation</th>
+              <th>Department</th>
+              <th>Type</th>
+              <th>Email</th>
+              <th>Extension</th>
+            </tr>
+          </thead>
+          <tbody>
+            {facultyData.map((faculty) => (
+              <tr key={faculty._id}>
+                <td>{faculty.name}</td>
+                <td>{faculty.designation}</td>
+                <td>{faculty.dept}</td>
+                <td>{faculty.type}</td>
+                <td>{faculty.email}</td>
+                <td>{faculty.extension}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default FacultyTable;
