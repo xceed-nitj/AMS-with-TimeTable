9d3332c96c4b76d762fb8fbc8ed9b3bb69f9a547
sarthaksharma39 <busharma2003@gamil.com>
2023-10-15 01:02:13 +0530

    added paragraph and align it to text position

--- src/certificateModule/Templates/01/Content.jsx
+++ src/certificateModule/Templates/01/Content.jsx
@@ -3,135 +3,142 @@ function Content({ data }) {
     return (
         <>
             <g textAnchor="middle">
-    <text
-      x="561.26"
-      y={186}
-      fill="#272727"
-      fontFamily="Butler-Bold"
-      fontSize={52}
-    >
-      CERTIFICATE
-    </text>
-    <text
-      x="561.26"
-      y={218}
-      fill="#272727"
-      fontFamily="AbhayaLibre-Regular"
-      fontSize={28}
-    >
-      OF ACHIEVEMENT
-    </text>
-    <text
-      x="561.26"
-      y={320}
-      fill="#272727"
-      fontFamily="AbhayaLibre-Regular"
-      fontSize={18}
-    >
-      THIS CERTIFICATE IS PRESENTED TO
-    </text>
-    <text
-      x="561.26"
-      y={400}
-      fill="#1E0C45"
-      fontFamily="Allura-Regular"
-      fontSize={80}
-    >
-      John Doe
-    </text>
-    <text
-      x="561.26"
-      y={455}
-      fill="#272727"
-      fontFamily="AbhayaLibre-Regular"
-      fontSize="18.64"
-    >
-      <tspan x="561.26">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-      </tspan>
-      <tspan x="561.26" dy={20}>
-        diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-      </tspan>
-      <tspan x="561.26" dy={20}>
-        aliquam erat volutpat. Ut wisi enim ad minim veniam,
-      </tspan>
-      <tspan x="561.26" dy={20}>
-        quis nos trud exerci tation ullamcorper suscipit lobortis nisl
-      </tspan>
-      <tspan x="561.26" dy={20}>
-        ut aliquip ex ea commodo consequat.
-      </tspan>
-    </text>
-    <text
-      x="361.342"
-      y={609}
-      fill="#272727"
-      fontFamily="AbhayaLibre-Regular"
-      fontSize={28}
-    >
-      15-Oct-2023
-    </text>
-    <text
-      x="361.342"
-      y={641}
-      fill="#272727"
-      fontFamily="AbhayaLibre-Regular"
-      fontSize={20}
-    >
-      DATE
-    </text>
-    <text
-      x="761.342"
-      y="608.859"
-      fill="#272727"
-      fontFamily="Dandelion-Script-Regular"
-      fontSize={38}
-    >
-      Jane Doe
-    </text>
-    <text
-      x="761.342"
-      y={641}
-      fill="#272727"
-      fontFamily="AbhayaLibre-Regular"
-      fontSize={20}
-    >
-      SIGNATURE
-    </text>
-    <text
-      x="980.401"
-      y="121.142"
-      fill="#FFFFFF"
-      fontFamily="Arvo-Bold"
-      fontSize={16}
-    >
-      BEST
-    </text>
-    <text
-      x="981.55"
-      y="139.142"
-      fill="#FFFFFF"
-      fontFamily="Arvo-Bold"
-      fontSize={16}
-    >
-      AWARD
-    </text>
-  </g>
+                <text
+                    x="561.26"
+                    y={186}
+                    fill="#272727"
+                    fontFamily="Butler-Bold"
+                    fontSize={52}
+                >
+                    CERTIFICATE
+                </text>
+                <text
+                    x="561.26"
+                    y={218}
+                    fill="#272727"
+                    fontFamily="AbhayaLibre-Regular"
+                    fontSize={28}
+                >
+                    OF ACHIEVEMENT
+                </text>
+                <text
+                    x="561.26"
+                    y={320}
+                    fill="#272727"
+                    fontFamily="AbhayaLibre-Regular"
+                    fontSize={18}
+                >
+                    THIS CERTIFICATE IS PRESENTED TO
+                </text>
+                <text
+                    x="561.26"
+                    y={400}
+                    fill="#1E0C45"
+                    fontFamily="Allura-Regular"
+                    fontSize={80}
+                >
+                    Sarthak Sharma
+                </text>
+                {/* <text
+                    x="561.26"
+                    y={455}
+                    fill="#272727"
+                    fontFamily="AbhayaLibre-Regular"
+                    fontSize="18.64"
+                >
+                    <tspan x="561.26">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                    </tspan>
+                    <tspan x="561.26" dy={20}>
+                        diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                    </tspan>
+                    <tspan x="561.26" dy={20}>
+                        aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                    </tspan>
+                    <tspan x="561.26" dy={20}>
+                        quis nos trud exerci tation ullamcorper suscipit lobortis nisl
+                    </tspan>
+                    <tspan x="561.26" dy={20}>
+                        ut aliquip ex ea commodo consequat.
+                    </tspan>
+                    
+                </text> */}
+                <text
+                    x="361.342"
+                    y={609}
+                    fill="#272727"
+                    fontFamily="AbhayaLibre-Regular"
+                    fontSize={28}
+                >
+                    15-Oct-2023
+                </text>
+                <text
+                    x="361.342"
+                    y={641}
+                    fill="#272727"
+                    fontFamily="AbhayaLibre-Regular"
+                    fontSize={20}
+                >
+                    DATE
+                </text>
+                <text
+                    x="761.342"
+                    y="608.859"
+                    fill="#272727"
+                    fontFamily="Dandelion-Script-Regular"
+                    fontSize={38}
+                >
+                    Prof. Binod
+                </text>
+                <text
+                    x="761.342"
+                    y={641}
+                    fill="#272727"
+                    fontFamily="AbhayaLibre-Regular"
+                    fontSize={20}
+                >
+                    SIGNATURE
+                </text>
+                <text
+                    x="980.401"
+                    y="121.142"
+                    fill="#FFFFFF"
+                    fontFamily="Arvo-Bold"
+                    fontSize={16}
+                >
+                    BEST
+                </text>
+                <text
+                    x="981.55"
+                    y="139.142"
+                    fill="#FFFFFF"
+                    fontFamily="Arvo-Bold"
+                    fontSize={16}
+                >
+                    AWARD
+                </text>
+            </g>
 
-            {/* <foreignObject x="50%"
+            <foreignObject 
+                x="70vw"
                 className="-translate-x-1/2"
-                y="465.314"
+                y="440"
                 fontSize="15.816"
                 textAnchor="middle"
                 fontFamily="AbhayaLibre-Regular"
-                width="200px"
-                height="200px"
+                width="500px"
+                height="100px"
+                // x="561.26"
+                //     y={455}
+                //     fill="#272727"
+                //     fontFamily="AbhayaLibre-Regular"
+                //     fontSize="18.64"
             >
-                <p className="w-full">HJNNZJ Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nemo sint odio delectus qui omnis voluptatibus illum amet consequuntur unde! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis a veniam dicta quibusdam similique ea sint quaerat consectetur nihil! Officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officiis quis architecto, laborum molestias facere explicabo laudantium inventore iste vitae! lrem30
+                <p className="w-full">This is a paragraph. Anything after height 100px while go disapper. HJNNZJ Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nemo sint odio delectus qui omnis voluptatibus illum amet consequuntur unde! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis a veniam dicta quibusdam similique ea sint quaerat consectetur nihil! Officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi officiis quis architecto, laborum molestias facere explicabo laudantium inventore iste vitae! lrem30
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati possimus unde dolorum, explicabo minus doloribus, quam molestiae esse eos quidem debitis vitae beatae libero nostrum odit tenetur in, vel officiis?
 
                 </p>
-            </foreignObject> */}
+            </foreignObject>
         </>
     )
 }
