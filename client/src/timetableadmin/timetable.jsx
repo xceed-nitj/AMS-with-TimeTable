2b02f9c1a45d3f73cfa722e56cb1e24855a86bc6
harimurugan1989 <harimur@gmail.com>
2023-10-18 18:34:46 +0530

    multiple post and fetch completed

--- client/src/timetableadmin/timetable.jsx
+++ client/src/timetableadmin/timetable.jsx
@@ -13,7 +13,9 @@ const Timetable = () => {
       try {
         const response = await fetch('http://127.0.0.1:8000/timetablemodule/tt/viewclasstt/abc-def-hij/3');
         const data = await response.json();
+       console.log(data);
         const initialData = generateInitialTimetableData(data);
+       
         setTimetableData(initialData);
       } catch (error) {
         console.error('Error fetching existing timetable data:', error);
@@ -30,23 +32,32 @@ const Timetable = () => {
     for (const day of days) {
       initialData[day] = {};
       for (const period of periods) {
-       const subj=fetchedData[day][`period${period}`][0][0].subject;
-       const room=fetchedData[day][`period${period}`][0][0].room;
-       const faculty=fetchedData[day][`period${period}`][0][0].faculty;
-       
-       const subInd=availableSubjects.indexOf(subj);
-       const roomInd=availableRooms.indexOf(room);
-       const facultyInd=availableFaculties.indexOf(faculty);
-        initialData[day][`period${period}`] = [
-          {
-            subject: availableSubjects[subInd],
-            room: availableRooms[roomInd],
-            faculty: availableFaculties[facultyInd],
-          },
-        ];
+        const slotData = fetchedData[day][`period${period}`];
+        initialData[day][`period${period}`] = [];
+  
+        for (const slot of slotData) {
+          const slotSubjects = [];
+  
+          for (const slotItem of slot) {
+            const subj = slotItem.subject;
+            const room = slotItem.room;
+            const faculty = slotItem.faculty;
+            const subInd = availableSubjects.indexOf(subj);
+            const roomInd = availableRooms.indexOf(room);
+            const facultyInd = availableFaculties.indexOf(faculty);
+  
+            slotSubjects.push({
+              subject: availableSubjects[subInd],
+              room: availableRooms[roomInd],
+              faculty: availableFaculties[facultyInd],
+            });
+          }
+  
+          initialData[day][`period${period}`].push(slotSubjects);
+        }
       }
     }
-
+  console.log(initialData);
     return initialData;
   };
 
@@ -62,28 +73,28 @@ const Timetable = () => {
     setTimetableData(updatedData);
   };
 
-  const handleSplitCell = (day, period) => {
+  const handleSplitCell = (day, period, slotIndex) => {
     const newCell = {
       subject: availableSubjects[0],
       room: availableRooms[0],
       faculty: availableFaculties[0],
     };
-
-    timetableData[day][`period${period}`].push(newCell);
-
+  
+    // Add the new cell to the specific slot within the day and period
+    timetableData[day][`period${period}`][slotIndex].push(newCell);
+  
+    // Update the state
     setTimetableData({ ...timetableData });
   };
-
-  const handleDeleteCell = (day, period, index) => {
-    const cellToDelete = timetableData[day][`period${period}`][index];
-
-    timetableData[day][`period${period}`] = timetableData[day][`period${period}`].filter(
-      (_, i) => i !== index
-    );
-
+  
+  const handleDeleteCell = (day, period, slotIndex, cellIndex) => {
+    // Remove the cell at the specified cellIndex within the specific slot
+    timetableData[day][`period${period}`][slotIndex].splice(cellIndex, 1);
+  
+    // Update the state
     setTimetableData({ ...timetableData });
   };
-
+  
   const handleSubmit = () => {
     const apiUrl = 'http://127.0.0.1:8000/timetablemodule/tt/savett';
     const code = 'abc-def-hij';
@@ -110,36 +121,37 @@ const Timetable = () => {
 
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-return (
-  <div>
-    <h1>TIME TABLE</h1>
-    {Object.keys(timetableData).length === 0 ? (
-      <div>Loading...</div> // or a loading spinner, message, etc.
-    ) : (
-     <table border="5" cellSpacing="0" align="center">
-      <tr>
-        <td align="center" height="50" width="100">
-          <b>Day/Period</b>
+  return (
+    <div>
+      <h1>TIME TABLE</h1>
+      {Object.keys(timetableData).length === 0 ? (
+  <div>Loading...</div>
+) : (
+  <table border="5" cellSpacing="0" align="center">
+    <tr>
+      <td align="center" height="50" width="100">
+        <b>Day/Period</b>
+      </td>
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((period) => (
+        <td key={period} align="center" height="50">
+          <b>{period}</b>
+        </td>
+      ))}
+    </tr>
+    {days.map((day) => (
+      <tr key={day}>
+        <td align="center" height="50">
+          <b>{day}</b>
         </td>
         {[1, 2, 3, 4, 5, 6, 7, 8].map((period) => (
           <td key={period} align="center" height="50">
-            <b>{period}</b>
-          </td>
-        ))}
-      </tr>
-      {days.map((day) => (
-        <tr key={day}>
-          <td align="center" height="50">
-            <b>{day}</b>
-          </td>
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((period) => (
-            <td key={period} align="center" height="50">
-              {timetableData[day][`period${period}`].map((cell, index) => (
-                <div key={index} className="cell-container">
-                  <div className="cell-slot">
+            {timetableData[day][`period${period}`].map((slot, slotIndex) => (
+              <div key={slotIndex} className="cell-container">
+                {slot.map((cell, cellIndex) => (
+                  <div key={cellIndex} className="cell-slot">
                     <select
                       value={cell.subject}
-                      onChange={(event) => handleCellChange(day, period, index, 'subject', event)}
+                      onChange={(event) => handleCellChange(day, period, slotIndex, cellIndex, 'subject', event)}
                     >
                       {availableSubjects.map((subjectOption) => (
                         <option key={subjectOption} value={subjectOption}>
@@ -149,7 +161,7 @@ return (
                     </select>
                     <select
                       value={cell.room}
-                      onChange={(event) => handleCellChange(day, period, index, 'room', event)}
+                      onChange={(event) => handleCellChange(day, period, slotIndex, cellIndex, 'room', event)}
                     >
                       {availableRooms.map((roomOption) => (
                         <option key={roomOption} value={roomOption}>
@@ -159,7 +171,7 @@ return (
                     </select>
                     <select
                       value={cell.faculty}
-                      onChange={(event) => handleCellChange(day, period, index, 'faculty', event)}
+                      onChange={(event) => handleCellChange(day, period, slotIndex, cellIndex, 'faculty', event)}
                     >
                       {availableFaculties.map((facultyOption) => (
                         <option key={facultyOption} value={facultyOption}>
@@ -168,31 +180,34 @@ return (
                       ))}
                     </select>
                   </div>
-                  <button
-                    className="cell-split-button"
-                    onClick={() => handleSplitCell(day, period)}
-                  >
-                    +
-                  </button>
-                  {index > 0 && (
-                    <button
-                      className="cell-delete-button"
-                      onClick={() => handleDeleteCell(day, period, index)}
-                    >
-                      Delete
-                    </button>
-                  )}
-                </div>
-              ))}
-            </td>
-          ))}
-        </tr>
-      ))}
-    </table>
+                ))}
+                 {slotIndex === 0 && (
+      <button
+        className="cell-split-button"
+        onClick={() => handleSplitCell(day, period, slotIndex)}
+      >
+        +
+      </button>
     )}
-    <button onClick={handleSubmit}>Save Timetable</button>
-    
-  </div>
-);
-                  }
+    {slotIndex === 0 && slot.length > 1 && (
+      <button
+        className="cell-delete-button"
+        onClick={() => handleDeleteCell(day, period, slotIndex)}
+      >
+        Delete
+      </button>
+    )}
+              </div>
+            ))}
+          </td>
+        ))}
+      </tr>
+    ))}
+  </table>
+)}
+      <button onClick={handleSubmit}>Save Timetable</button>
+    </div>
+  );
+};
+
 export default Timetable;
