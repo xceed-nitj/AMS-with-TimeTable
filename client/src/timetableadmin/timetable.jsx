40dd8b30af42af9682a214c5713617bde1ad5cb4
harimurugan1989 <harimur@gmail.com>
2023-10-18 15:14:28 +0530

    get request for single cell cases working

--- client/src/timetableadmin/timetable.jsx
+++ client/src/timetableadmin/timetable.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-<link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-  integrity="sha384-pzjw2HEZBrnHL2hZCBK7W6bzXNEquXpDfXSt8zytUGhSfwmkDr/jwGAqR6u6I0rt"
-  crossorigin="anonymous"
-/>
+import React, { useState, useEffect } from 'react';
 
-class Timetable extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      timetableData: {},
-      availableSubjects: ['Eng', 'Mat', 'Che', 'Phy', 'Other'],
-      availableRooms: ['Room1', 'Room2', 'Room3', 'Room4', 'Room5'],
-      availableFaculties: ['Faculty1', 'Faculty2', 'Faculty3', 'Faculty4', 'Faculty5'],
-      selectedCell: null,
+const Timetable = () => {
+  const [timetableData, setTimetableData] = useState({});
+  const availableSubjects = ['Eng', 'Mat', 'Che', 'Phy', 'Other'];
+  const availableRooms = ['Room1', 'Room2', 'Room3', 'Room4', 'Room5'];
+  const availableFaculties = ['Faculty1', 'Faculty2', 'Faculty3', 'Faculty4', 'Faculty5'];
+  const selectedCell = null;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/timetablemodule/tt/viewclasstt/abc-def-hij/3');
+        const data = await response.json();
+        const initialData = generateInitialTimetableData(data);
+        setTimetableData(initialData);
+      } catch (error) {
+        console.error('Error fetching existing timetable data:', error);
+      }
     };
-    this.state.timetableData = this.generateInitialTimetableData();
-  }
+    fetchData();
+  }, []);
 
-  generateInitialTimetableData() {
+  const generateInitialTimetableData = (fetchedData) => {
     const initialData = {};
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
     const periods = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -28,211 +30,169 @@ class Timetable extends Component {
     for (const day of days) {
       initialData[day] = {};
       for (const period of periods) {
+       const subj=fetchedData[day][`period${period}`][0][0].subject;
+       const room=fetchedData[day][`period${period}`][0][0].room;
+       const faculty=fetchedData[day][`period${period}`][0][0].faculty;
+       
+       const subInd=availableSubjects.indexOf(subj);
+       const roomInd=availableRooms.indexOf(room);
+       const facultyInd=availableFaculties.indexOf(faculty);
         initialData[day][`period${period}`] = [
           {
-            subject: this.state.availableSubjects[0],
-            room: this.state.availableRooms[0],
-            faculty: this.state.availableFaculties[0],
+            subject: availableSubjects[subInd],
+            room: availableRooms[roomInd],
+            faculty: availableFaculties[facultyInd],
           },
         ];
       }
     }
 
     return initialData;
-  }
-
- async getbackendData() {
-  try {
-    const response = await fetch('http://127.0.0.1:8000/timetablemodule/tt/viewclasstt/abc-def-hij/3');
-    const data = await response.json();
-
-    if (data) {
-      this.setState({ timetableData: data });
-    } else {
-      // Handle the case where data is not available
-      console.error('No data available.');
-    }
-  } catch (error) {
-    console.error('Error fetching existing timetable data:', error);
-  }
-}
-
-
-  handleCellChange = (day, period, index, type, event) => {
-    const newValue = event.target.value;
-    const updatedData = { ...this.state.timetableData };
-    updatedData[day][`period${period}`][index][type] = newValue;
-    this.setState({ timetableData: updatedData });
   };
 
-  handleSplitCell = (day, period) => {
-    const { timetableData } = this.state;
+  useEffect(() => {
+    console.log('Updated timetableData:', timetableData);
+  }, [timetableData]);
 
-    // Create a new cell with the same data as the original cell
+
+  const handleCellChange = (day, period, index, type, event) => {
+    const newValue = event.target.value;
+    const updatedData = { ...timetableData };
+    updatedData[day][`period${period}`][index][type] = newValue;
+    setTimetableData(updatedData);
+  };
+
+  const handleSplitCell = (day, period) => {
     const newCell = {
-      subject: this.state.availableSubjects[0],
-      room: this.state.availableRooms[0],
-      faculty: this.state.availableFaculties[0],
+      subject: availableSubjects[0],
+      room: availableRooms[0],
+      faculty: availableFaculties[0],
     };
 
-    // Add the new cell to the current period
     timetableData[day][`period${period}`].push(newCell);
 
-    // Update the state with the new timetable data
-    this.setState({ timetableData });
-  };  
+    setTimetableData({ ...timetableData });
+  };
 
-  handleDeleteCell = (day, period, index) => {
-    const { timetableData } = this.state;
-  
-    // Find the cell to delete
+  const handleDeleteCell = (day, period, index) => {
     const cellToDelete = timetableData[day][`period${period}`][index];
-  
-    // Remove the cell at the specified index
+
     timetableData[day][`period${period}`] = timetableData[day][`period${period}`].filter(
       (_, i) => i !== index
     );
-  
-    // Update the state with the modified timetable data
-    this.setState({ timetableData });
-  
-    // If needed, you can also handle further actions, such as saving the updated data to the backend.
-  };
-  
-  
-  handleSubmit = () => {
-    const apiUrl = 'http://127.0.0.1:8000/timetablemodule/tt/savett'; // Replace with the actual URL of your backend route
-    const { timetableData } = this.state;
-    const code='abc-def-hij';
-    const sem='3';
-      // Convert the data to JSON
-      const dataToSend = JSON.stringify({ timetableData,code });
 
-      // Log the JSON data to the console
-      console.log('JSON Data to Send:', dataToSend);
+    setTimetableData({ ...timetableData });
+  };
+
+  const handleSubmit = () => {
+    const apiUrl = 'http://127.0.0.1:8000/timetablemodule/tt/savett';
+    const code = 'abc-def-hij';
+    const sem = '3';
+    const dataToSend = JSON.stringify({ timetableData, code });
+
+    console.log('JSON Data to Send:', dataToSend);
 
     fetch(apiUrl, {
-      method: 'POST', // or 'PUT' or 'PATCH' if needed
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ timetableData,code,sem }),
+      body: JSON.stringify({ timetableData, code, sem }),
     })
       .then(response => response.json())
       .then(data => {
         console.log('Data sent to the backend:', data);
-        // You can handle the response from the backend here
       })
       .catch(error => {
         console.error('Error sending data to the backend:', error);
-        // Handle any errors here
       });
   };
 
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-  render() {
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-
-    return (
-      <div>
-        <h1>TIME TABLE</h1>
-        <table border="5" cellSpacing="0" align="center">
-          <tr>
-            <td align="center" height="50" width="100">
-              <b>Day/Period</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>8:30-9:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>9:30-10:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>10:30-11:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>11:30-12:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>1:30-2:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>2:30-3:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>3:30-4:30</b>
-            </td>
-            <td align="center" height="50" width="100">
-              <b>4:30-5:30</b>
-            </td>
-          </tr>
-          {days.map((day) => (
-            <tr key={day}>
-              <td align="center" height="50">
-                <b>{day}</b>
-              </td>
-              {[1, 2, 3, 4, 5, 6, 7, 8].map((period) => (
-                <td key={period} align="center" height="50">
-                  {this.state.timetableData[day][`period${period}`].map((subjectData, index) => (
-                    <div key={index} className="cell-container">
-                      <div className="cell-slot">
-                        <select
-                          value={subjectData.subject}
-                          onChange={(event) => this.handleCellChange(day, period, index, 'subject', event)}
-                        >
-                          {this.state.availableSubjects.map((subjectOption) => (
-                            <option key={subjectOption} value={subjectOption}>
-                              {subjectOption}
-                            </option>
-                          ))}
-                        </select>
-                        <select
-                          value={subjectData.room}
-                          onChange={(event) => this.handleCellChange(day, period, index, 'room', event)}
-                        >
-                          {this.state.availableRooms.map((roomOption) => (
-                            <option key={roomOption} value={roomOption}>
-                              {roomOption}
-                            </option>
-                          ))}
-                        </select>
-                        <select
-                          value={subjectData.faculty}
-                          onChange={(event) => this.handleCellChange(day, period, index, 'faculty', event)}
-                        >
-                          {this.state.availableFaculties.map((facultyOption) => (
-                            <option key={facultyOption} value={facultyOption}>
-                              {facultyOption}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                      <button
-                        className="cell-split-button"
-                        onClick={() => this.handleSplitCell(day, period)}
-                      >
-                        +
-                      </button>
-                      {index > 0 && (
+return (
+  <div>
+    <h1>TIME TABLE</h1>
+    {Object.keys(timetableData).length === 0 ? (
+      <div>Loading...</div> // or a loading spinner, message, etc.
+    ) : (
+     <table border="5" cellSpacing="0" align="center">
+      <tr>
+        <td align="center" height="50" width="100">
+          <b>Day/Period</b>
+        </td>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((period) => (
+          <td key={period} align="center" height="50">
+            <b>{period}</b>
+          </td>
+        ))}
+      </tr>
+      {days.map((day) => (
+        <tr key={day}>
+          <td align="center" height="50">
+            <b>{day}</b>
+          </td>
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((period) => (
+            <td key={period} align="center" height="50">
+              {timetableData[day][`period${period}`].map((cell, index) => (
+                <div key={index} className="cell-container">
+                  <div className="cell-slot">
+                    <select
+                      value={cell.subject}
+                      onChange={(event) => handleCellChange(day, period, index, 'subject', event)}
+                    >
+                      {availableSubjects.map((subjectOption) => (
+                        <option key={subjectOption} value={subjectOption}>
+                          {subjectOption}
+                        </option>
+                      ))}
+                    </select>
+                    <select
+                      value={cell.room}
+                      onChange={(event) => handleCellChange(day, period, index, 'room', event)}
+                    >
+                      {availableRooms.map((roomOption) => (
+                        <option key={roomOption} value={roomOption}>
+                          {roomOption}
+                        </option>
+                      ))}
+                    </select>
+                    <select
+                      value={cell.faculty}
+                      onChange={(event) => handleCellChange(day, period, index, 'faculty', event)}
+                    >
+                      {availableFaculties.map((facultyOption) => (
+                        <option key={facultyOption} value={facultyOption}>
+                          {facultyOption}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <button
+                    className="cell-split-button"
+                    onClick={() => handleSplitCell(day, period)}
+                  >
+                    +
+                  </button>
+                  {index > 0 && (
                     <button
                       className="cell-delete-button"
-                      onClick={() => this.handleDeleteCell(day, period, index)}
+                      onClick={() => handleDeleteCell(day, period, index)}
                     >
                       Delete
                     </button>
                   )}
-                    </div>
-                  ))}
-                </td>
+                </div>
               ))}
-            </tr>
+            </td>
           ))}
-        </table>
-        <button onClick={this.handleSubmit}>Save Timetable</button>
-      </div>
-    );
+        </tr>
+      ))}
+    </table>
+    )}
+    <button onClick={handleSubmit}>Save Timetable</button>
     
-  }
-}
-
+  </div>
+);
+                  }
 export default Timetable;
