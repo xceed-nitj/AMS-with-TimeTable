1bf283c31128990022d80139f1ccf653dedddab9
harimurugan1989 <harimur@gmail.com>
2023-10-25 13:55:39 +0530

    typo corrected in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <Route path="/" element={<CreateTimetable />} />
           <Route path="/tt/:generatedLink/addfaculty" element={<AddFaculty />} />
           <Route path="/tt/masterfaculty" element={<MasterFaculty />} />
-          <Route path="/tt/masterroom" element={<MasterRoom />} /
+          <Route path="/tt/masterroom" element={<MasterRoom />} />
           <Route path="/tt/:generatedLink/lockedsummary" element={<LockedSummary />} />
         </Routes>
       </div>
