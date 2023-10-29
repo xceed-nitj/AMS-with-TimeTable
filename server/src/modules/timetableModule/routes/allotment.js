7c6bdc6b75cf7f16ad9183d8d11abd526a1306d1
Akshika Garg <akshikagarg108@gmail.com>
2023-10-29 15:55:19 +0530

    allotment routes completed

---
+++ server/src/modules/timetableModule/routes/allotment.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const allotmentRouter = express.Router();
+const AllotmentController = require("../controllers/allotment");
+const allotmentController = new AllotmentController();
+
+allotmentRouter.post("/", async (req, res) => {
+    try {
+      await allotmentController.AddAllotment(req, res);
+    } catch (e) {
+      res
+        .status(e?.status || 500)
+        .json({ error: e?.message || "Internal Server Error" });
+    }
+  });
+
+  allotmentRouter.get("/", async (req, res) => {
+    try {
+      await allotmentController.getAllotment(req,res) ;
+    } catch (e) {
+      res
+        .status(e?.status || 500)
+        .json({ error: e?.message || "Internal Server Error" });
+    }
+  });
+
+  allotmentRouter.get("/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const resp = await allotmentController.getAllotmentById(id);
+      res.status(200).json(resp);
+    } catch (e) {
+      res
+        .status(e?.status || 500)
+        .json({ error: e?.message || "Internal Server Error" });
+    }
+  });
+
+  allotmentRouter.get('/session', async (req, res) => {
+    try {
+      const sessions = await allotmentController.getSessions(); 
+      res.status(200).json(sessions);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  allotmentRouter.put('/:id', async (req, res) => {
+      try {
+        const allotmentID = req.params.id;
+        const updatedId = req.body;
+        await allotmentController.updateID(
+          allotmentID,updatedId
+        );
+        res.status(200).json({ response: "Allotment updated successfully" });
+      } catch (e) {
+        res
+          .status(e?.status || 500)
+          .json({ error: e?.message || "Internal Server Error" });
+      }
+    });
+
+    allotmentRouter.delete("/:id", async (req, res) => {
+      try {
+        const allotmentID = req.params.id;
+        await allotmentController.deleteId(allotmentID);
+        res.status(200).json({ response: "Allotment deleted successfully" });
+      } catch (e) {
+        res
+          .status(e?.status || 500)
+          .json({ error: e?.message || "Internal Server Error" });
+      }
+    });
+
+  module.exports = allotmentRouter;
