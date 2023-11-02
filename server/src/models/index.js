0be0efb88037c77707d0f798cf6a4fc62995c6ff
pihu24 <pihu24aru20pr@gmail.com>
2023-11-02 14:22:34 +0530

    trying

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,7 +5,7 @@ import * as Student from './student';
 import * as addFaculty from './addfaculty';
 import * as LockSem from './locksem';
 import * as User from './usermanagement/user';
-import * as LockFaculty from './lockfacultysummary';
+import * as LockFaculty from './lockfaculty';
 
 export {
     Subject,Table,Faculty,Student,addFaculty, LockSem, User,LockFaculty
