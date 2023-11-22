9260094c0a2ae82e6c7774045da6a7586ebd9c92
harimurugan1989 <harimur@gmail.com>
2023-11-22 15:19:32 +0530

    mater view added

--- client/src/timetableadmin/mastersearch.jsx
+++ client/src/timetableadmin/mastersearch.jsx
@@ -512,7 +512,7 @@ useEffect(()=>
       </Box>
       {/* Faculty Dropdown */}
       <FormControl>
-        <FormLabel fontWeight='bold'>Faculty timetable (locked)</FormLabel>
+        <FormLabel fontWeight='bold'>Faculty timetable (yet to be completed)</FormLabel>
         <Select
           value={selectedFaculty}
           onChange={(e) => setSelectedFaculty(e.target.value)}
@@ -575,7 +575,7 @@ useEffect(()=>
           )}
       </Box>
     <FormControl>
-     <FormLabel fontWeight='bold' >Room timetable (locked)</FormLabel>
+     <FormLabel fontWeight='bold' >Room timetable (yet to be completed)</FormLabel>
       {/* Room Dropdown */}
       <Select
         value={selectedRoom}
