aebec3416949f03d02cda7ad02ac6ac5879897bc
harimurugan1989 <harimur@gmail.com>
2023-11-25 07:17:53 +0530

    login page updated

--- client/src/dashboard/login.jsx
+++ client/src/dashboard/login.jsx
@@ -1,23 +1,23 @@
 // src/Login.js
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import getEnvironment from "../getenvironment";
-import './login.css'
+import { Box, Button, Input, Text, VStack, Center, Image } from "@chakra-ui/react";
 import logoImage from '../assets/logo.png'; 
 import logoVideo from '../assets/video.mp4'; 
+import { CustomBlueButton } from "../styles/customStyles";
+
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const apiUrl=getEnvironment();
-
-
+  const [message, setMessage] = useState('');
+  const apiUrl = getEnvironment();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const userData = { email, password };
-    // console.log(userData)
-    // console.log(apiUrl)
+
     try {
       const response = await fetch(`${apiUrl}/auth/login`, {
         method: 'POST',
@@ -25,78 +25,68 @@ function Login() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(userData),
-        credentials: 'include'
+        credentials: 'include',
       });
 
       if (response.ok) {
         const responseData = await response.json();
-        console.log(responseData.message);
-        // Redirect to the dashboard page on successful login
+        setMessage(responseData.message);
         window.location.href = '/dashboard';
       } else {
-        // Handle login errors, e.g., display an error message
-        console.error('Login failed');
+        const errorData = await response.json();
+        setMessage(`Login failed: ${errorData.message}`);
       }
     } catch (error) {
       console.error('An error occurred', error);
+      setMessage('An error occurred. Please try again.');
     }
   };
 
   return (
+    <Box bg="black" color="white" minHeight="100vh" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Box display="flex" flexWrap="wrap" justifyContent="center" textAlign="center">
+        {/* Video Section */}
+        <Box flex="1" width={{ base: '100%', md: '70%' }} mb={{ base: 4, md: 0 }}>
+          <video className="video" autoPlay loop muted style={{ width: '100%' }}>
+            <source src={logoVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </Box>
 
-<div className="App">
-<video className="video" autoPlay loop muted>
-      <source src={logoVideo} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
-          {/* <div className="appAside" /> */}
-         
-          
-          <div className="appForm">
+        {/* Second Section (Login Form) */}
+        <Center flex="1" display="flex" flexDirection="column" alignItems="center" width={{ base: '100%', md: '100%' }}>
+          {/* Conditional Rendering of Logo */}
+          <Image src={logoImage} alt="Your Logo" display={{ base: 'none', md: 'block' }} mb={4} />
 
-          <div className="formCenter">
-          <div className="logoContainer">
-                <img src={logoImage} alt="Your Logo" className="logoImage" />
-              </div>       
-    <form className="formFields" onSubmit={handleSubmit}>
-      <div className="formField">
-     
-        <label className="formFieldLabel" htmlFor="email">
-          E-Mail Address
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="formFieldInput"
-          placeholder="Enter your email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4} mt={{ base: 10, md: 0 }} width="100%" maxW="700px">
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                isRequired
+              />
+              <CustomBlueButton type="submit" width="80%">
+                Login
+              </CustomBlueButton>
+            </VStack>
+          </form>
 
-      <div className="formField">
-        <label className="formFieldLabel" htmlFor="password">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="formFieldInput"
-          placeholder="Enter your password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-    />
-      </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-
-
-</div>
-          </div>
+          {message && (
+            <Text mt={4}>
+              {message}
+            </Text>
+          )}
+        </Center>
+      </Box>
+    </Box>
   );
 }
 
