edd64edb2abe4d76db8957319dacc653b50b5cc8
harimurugan1989 <harimur@gmail.com>
2023-11-25 06:44:46 +0530

    checking time

--- server/src/modules/timetableModule/helper/getIndianTime.js
+++ server/src/modules/timetableModule/helper/getIndianTime.js
@@ -1,29 +1,25 @@
-const getIndianTime = async (timestamp) => {
+const getIndianTime = (timestamp) => {
   const date = new Date(timestamp);
 
   // Specify the IST offset in minutes (GMT+5:30)
-  const ISTOffsetMinutes = 0; // 5 hours and 30 minutes
+  const ISTOffsetMinutes = 330; // 5 hours and 30 minutes
 
-  // Calculate the UTC timestamp with the IST offset
-  const utcTimestamp = date.getTime() + (ISTOffsetMinutes * 60000);
+  // Set the UTC hours and minutes with the IST offset
+  date.setUTCHours(date.getUTCHours() + ISTOffsetMinutes / 60);
+  date.setUTCMinutes(date.getUTCMinutes() + ISTOffsetMinutes % 60);
 
-  // Create a new Date object with the adjusted UTC timestamp
-  const istDate = new Date(utcTimestamp);
-
-  // Format the date in IST to 'DD/MM/YYYY' format
-  const day = String(istDate.getDate()).padStart(2, '0');
-  const month = String(istDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-  const year = istDate.getFullYear();
-
-  // Format the time in IST in HH:MM:SS AM/PM format
-  const hours = String(istDate.getHours() % 12 || 12).padStart(2, '0');
-  const minutes = String(istDate.getMinutes()).padStart(2, '0');
-  const seconds = String(istDate.getSeconds()).padStart(2, '0');
-  const ampm = istDate.getHours() < 12 ? 'AM' : 'PM';
-
-  const formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
+  const formattedDateTime = date.toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  });
 
   return formattedDateTime;
-}
+};
 
 module.exports = getIndianTime;
