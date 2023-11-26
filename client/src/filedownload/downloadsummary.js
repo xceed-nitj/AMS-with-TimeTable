34d4e34094bf92446b4b53982b2c3511591058cb
harimurugan1989 <harimur@gmail.com>
2023-11-26 11:24:44 +0530

    s.no and border updated

--- client/src/filedownload/downloadsummary.js
+++ client/src/filedownload/downloadsummary.js
@@ -4,12 +4,12 @@ import header from '../assets/header.png';
 import footer from '../assets/footer.png';
 import { CustomTh, CustomLink, CustomBlueButton } from '../styles/customStyles';
 
-// pdfMake.vfs = pdfFonts.pdfMake.vfs;
-pdfMake.vfs=pdfFonts && pdfFonts.pdfMake ? pdfFonts.pdfMake.vfs : globalThis.pdfMake.vfs;
-const generateSummaryTablePDF = (allFacultySummaries,session,dept) => {
+pdfMake.vfs = pdfFonts && pdfFonts.pdfMake ? pdfFonts.pdfMake.vfs : globalThis.pdfMake.vfs;
+
+const generateSummaryTablePDF = (allFacultySummaries, session, dept) => {
   const facultyTotals = new Map();
   const rows = [];
-  let totalRowsCount = 0;
+  let sNo = 0;
 
   allFacultySummaries.forEach((facultyObj, index) => {
     const faculty = facultyObj.faculty;
@@ -23,25 +23,27 @@ const generateSummaryTablePDF = (allFacultySummaries,session,dept) => {
     facultyTotals.set(faculty, totalHrs);
 
     const facultyRows = summaryData.map((summary, subIndex) => {
-      totalRowsCount += 1;
+      const isFirstRow = subIndex === 0;
+
+      // Increment sNo only for the first row of each faculty
+      sNo = isFirstRow ? sNo + 1 : sNo;
 
       const row = [
-        { text: totalRowsCount, rowSpan: subIndex === 0 ? summaryData.length : 1, alignment: 'center' },
-        { text: subIndex === 0 ? faculty : '', rowSpan: subIndex === 0 ? summaryData.length : 1},
+        { text: isFirstRow ? sNo : '', rowSpan: isFirstRow ? summaryData.length : 1, alignment: 'center' },
+        { text: isFirstRow ? faculty : '', rowSpan: isFirstRow ? summaryData.length : 1 },
         { text: summary.subSem || '', alignment: 'center' },
         { text: summary.subCode || '', alignment: 'center' },
         { text: summary.subjectFullName || '', alignment: 'left' },
         { text: summary.subType || '', alignment: 'center' },
-        { text: summary.count || 0, alignment: 'center' }, // Assuming 'count' represents the number of hours for each entry
-        { text: totalHrs, rowSpan: summaryData.length, alignment: 'center' },
+        { text: summary.count || 0, alignment: 'center' },
+        { text: isFirstRow ? totalHrs : '', rowSpan: isFirstRow ? summaryData.length : 1, alignment: 'center' },
       ];
-      
+
       return row;
     });
 
     rows.push(...facultyRows);
   });
-
 
   const headerImage = new Image();
   headerImage.src = header;
@@ -65,72 +67,65 @@ const generateSummaryTablePDF = (allFacultySummaries,session,dept) => {
       footerContext.drawImage(footerImage, 0, 0);
       const footerImageDataURL = footerCanvas.toDataURL('image/png');
 
-
-  const docDefinition = {
-    pageOrientation: 'landscape',
-    header: {
-      image: headerImageDataURL,
-      width: 300,
-      alignment: 'center',
-    },
-    footer: {
-      image: footerImageDataURL,
-      width: 250,
-      alignment: 'center',
-    },
-    content: [
-      {
-        text: `Department of ${dept}`,
-        fontSize: 12,
-        bold: true,
-        margin: [5, 0, 40, 0],
-        alignment: 'center',
-      },
-      { text: '\n' },
-      { text: `Faculty Load Allocation for the session ${session}`,  alignment: 'center'},
-      { text: '\n' },
-      {
-        table: {
-          headerRows: 1,
-          widths: [30, 100, 90, 90, 180, '*',30, 50],
-        alignment: 'center',
-
-          body: [
-            [
-              { text: 'S.No', alignment: 'center' },
-              { text: 'Faculty Name', alignment: 'center' },
-              { text: 'Semester', alignment: 'center' },
-              { text: 'Subject Code', alignment: 'center' },
-              { text: 'Subject Name', alignment: 'center' },
-              { text: 'Type', alignment: 'center' },
-              { text: 'Hrs', alignment: 'center' },
-              { text: 'Total Hrs', alignment: 'center' },
-            ],
-            ...rows,
-
-          ],
-
+      const docDefinition = {
+        pageOrientation: 'landscape',
+        header: {
+          image: headerImageDataURL,
+          width: 300,
+          alignment: 'center',
         },
-      },
-      
-    ],
-    styles: {
-      header: {
-        fontSize: 18,
-        bold: true,
-        alignment: 'center',
-      },
-    },
+        footer: function (currentPage, pageCount) {
+          // Draw a line at the bottom of each page
+          return {
+            canvas: [
+              { type: 'line', x1: 40, y1: 780, x2: 560, y2: 780, lineWidth: 1, lineColor: 'black' },
+            ],
+          };
+        },
+        content: [
+          {
+            text: `Department of ${dept}`,
+            fontSize: 12,
+            bold: true,
+            margin: [5, 0, 40, 0],
+            alignment: 'center',
+          },
+          { text: '\n' },
+          { text: `Faculty Load Allocation for the session ${session}`, alignment: 'center' },
+          { text: '\n' },
+          {
+            table: {
+              headerRows: 1,
+              widths: [30, 150, '*', '*', '*', '*', 30, 50],
+              alignment: 'center',
+              body: [
+                [
+                  { text: 'S.No', alignment: 'center' },
+                  { text: 'Faculty Name', alignment: 'center' },
+                  { text: 'Semester', alignment: 'center' },
+                  { text: 'Subject Code', alignment: 'center' },
+                  { text: 'Subject Name', alignment: 'center' },
+                  { text: 'Type', alignment: 'center' },
+                  { text: 'Hrs', alignment: 'center' },
+                  { text: 'Total Hrs', alignment: 'center' },
+                ],
+                ...rows,
+              ],
+            },
+          },
+        ],
+        styles: {
+          header: {
+            fontSize: 18,
+            bold: true,
+            alignment: 'center',
+          },
+        },
+      };
 
-    
+      pdfMake.createPdf(docDefinition).open();
+    };
   };
-
-  // Use pdfmake to open the PDF document
-  pdfMake.createPdf(docDefinition).open();
 };
-
-};
-}
-
 
 export default generateSummaryTablePDF;
