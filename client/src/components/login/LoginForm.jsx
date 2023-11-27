afecbc85b4dff54625aa3f284cca486351bfe7fd
harimurugan1989 <harimur@gmail.com>
2023-11-27 16:43:52 +0530

    rolespage removed

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -37,7 +37,7 @@ const LoginForm = () => {
       if (response.ok) {
         const responseData = await response.json()
         setMessage(responseData.message)
-        window.location.href = '/userroles'
+        window.location.href = '/tt/dashboard'
       } else {
         const errorData = await response.json()
         setMessage(`Login failed: ${errorData.message}`)
