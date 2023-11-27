bfc899c65200704d769b472622bae5a6a1624dfa
harimurugan1989 <harimur@gmail.com>
2023-11-27 14:52:32 +0530

    logout updated

---
+++ server/src/modules/usermanagement/controllers/usercontroller.js
@@ -0,0 +1,29 @@
+const HttpException = require("../../../models/http-exception");
+const User = require("../../../models/usermanagement/user");
+
+class UserController {
+      async  getUserDetails(req, res) {
+        try {
+          const userId = req.user.id;
+          const user = await User.findById(userId);
+          if (!user) {
+            throw { status: 404, message: "User not found" };
+          }
+          res.json({ user });  
+        } catch (error) {
+          console.error(error); 
+          res.status(500).json({ error: "Internal server error" });
+        }
+      }
+
+
+      
+     
+    }
+
+
+
+
+module.exports = UserController;
+
+
