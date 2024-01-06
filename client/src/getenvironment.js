1759fee10a125cfd35fe2f16d4ef39bd51a727f9
Gautam Singla <ggsingla777@gmail.com>
2024-01-06 23:36:51 +0530

    fix-env-file

--- client/src/getenvironment.js
+++ client/src/getenvironment.js
@@ -1,19 +1,18 @@
 // envUtils.js
 function getEnvironment() {
-  // const currentURL = window.location.href;
-  // const development='http://localhost:8010'
-  // const production='https://nitjtt.onrender.com'
+  const currentURL = window.location.href;
+  const development = 'http://localhost:8010';
+  const production = 'https://nitjtt.onrender.com';
   const nitjServer = 'https://xceed.nitj.ac.in';
 
-  // if (currentURL.includes('localhost')) {
-  //   return development;
-  // } else if (currentURL.includes('nitjtt')) {
-  //   return production;
-  // } else {
-  //   // Default to a specific environment or handle other cases
-  //   return nitjServer;
-  // }
-  return nitjServer;
+  if (currentURL.includes('localhost')) {
+    return development;
+  } else if (currentURL.includes('nitjtt')) {
+    return production;
+  } else {
+    // Default to a specific environment or handle other cases
+    return nitjServer;
+  }
 }
 
 export default getEnvironment;
