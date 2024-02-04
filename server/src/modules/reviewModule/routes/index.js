12f7703bd7970591b3e0cc4b2bc80e818d3a7433
Ajay <aky11052003@gmail.com>
2024-02-04 22:54:17 +0530

    added reuploaded

--- server/src/modules/reviewModule/routes/index.js
+++ server/src/modules/reviewModule/routes/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fileUploadMiddleware = require('../controller/uploadFileMiddleWare.js');
 const { findPaper, findAllPapers, updatePaper } = require('../controller/papers.js');
 const uploadPaper = require('../controller/uploadFile.js');
+const reupload = require('../controller/reupload.js');
 
 const router = express.Router();
 
@@ -14,6 +15,10 @@ router.get('/papers', findAllPapers)
 router.get('/paper/:id', findPaper)   // To find paper using paperId (not _id)
 router.patch('/paper/:id', updatePaper)  // To update paper using paperId (not _id)
 router.post('/upload', fileUploadMiddleware, uploadPaper);
+router.get('/delete', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index2.html'));
+});
+router.post('/delete/:id', reupload);
 
 router.use('/reviewer', require('./reviewer.js'));
 router.use('/user', require('./user.js'));
