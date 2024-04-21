d00b2e7a3b6d90ba31732997b9a279d67f49dbf5
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2024-04-22 03:54:10 +0530

    Revert "added dummy editor dashboard"

--- server/src/modules/reviewModule/controller/reviewer.js
+++ server/src/modules/reviewModule/controller/reviewer.js
@@ -14,15 +14,6 @@ app.use(
 );
 app.use(bodyParser.json());
 
-const getAllReviewers = async (req, res) => {
-  try {
-    const user = await User.find({ role: "Reviewer" }).exec();
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error", error: error.message });
-  }
-}
-
 const addReviewer = async (req, res) => {
   let { paperId, userId } = req.query;
   console.log(paperId, userId);
@@ -37,10 +28,10 @@ const addReviewer = async (req, res) => {
 
     const user = await User.findById(userId);
 
-    // if (!SignedInUser) {
-    //   res.status(401).send("User not found");
-    //   return;
-    // }
+    if (!user) {
+      res.status(401).send("User not found");
+      return;
+    }
 
     if (user.role !== "Editor") {
       res.status(401).send("Only editor is allowed to add reviewer");
@@ -165,4 +156,4 @@ const deleteReviewer = async (req, res) => {
   }
 };
 
-module.exports = { getAllReviewers, addReviewer, updateReviewer, deleteReviewer };
+module.exports = { addReviewer, updateReviewer, deleteReviewer };
--- server/src/modules/reviewModule/routes/reviewer.js
+++ server/src/modules/reviewModule/routes/reviewer.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { addReviewer, updateReviewer, deleteReviewer } = require('../controller/reviewer');
-const { addReviewer, updateReviewer, deleteReviewer, getAllReviewers } = require('../controller/reviewer');
 
 const router = express.Router();
 
-router.get('/', getAllReviewers);
 router.post('/addReviewer', addReviewer);
 router.patch('/updateReview', updateReviewer);
 router.delete('/deleteReviewer', deleteReviewer);
+
+module.exports = router;
