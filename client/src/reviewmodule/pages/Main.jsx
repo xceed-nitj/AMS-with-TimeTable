58ef89cfeb1fbb1b9a16153464b71f1252425a5b
harimurugan1989 <harimur@gmail.com>
2024-05-22 00:04:13 +0530

    editor dashboard page updated

--- client/src/reviewmodule/pages/Main.jsx
+++ client/src/reviewmodule/pages/Main.jsx
@@ -6,11 +6,7 @@ import { Box } from '@chakra-ui/react';
 // import Settings from './Settings';
 // import About from './About';
 // import Author1 from './Author1';
-import MultiStepForm from './MultiStepForm';
-import EventForm from './editorevent';
-import AddReviewer from './AddReviewer';
-import PrmEditorDashboard from './PrmEditorDashboard';
-import PRMDashboard from './prmdashboard';
+
 import getEnvironment from "../../getenvironment";
 
 
@@ -64,14 +60,34 @@ function HomePage() {
 
     fetchEditorSubmenu();
   }, []);
-
+  let content;
+  switch (activeTab) {
+    case 'Home':
+    //   content = <Home />;
+      break;
+    case 'Author':
+    //   content = <About />;
+      break;
+    case 'New Submission':
+      content = <MultiStepForm />;
+      break;
+    case 'Papers Submitted':
+    //   content = <Author2 />;
+      break;
+    case 'Event Dashboard':
+      content = <PRMDashboard />;
+      break;
+    // Add more cases as needed
+    // default:
+    //   content = <Home />;
+  }
   return (
     <Box display="flex">
       <SideBarFinal/>
       <Box ml="250px" p={4} flex="1">
         {content}
       </Box>
-    </Box>
+        </Box>
   );
 }
 
