c4a5713f4942bf4e472f0323d91136456e69476e
goyalrachit15 <rachitgoyal37@gmail.com>
2024-06-10 17:34:35 +0530

    deleted UpdateTemplate and made use of updateEvent

--- server/src/modules/reviewModule/controller/event.js
+++ server/src/modules/reviewModule/controller/event.js
@@ -88,23 +88,6 @@ const getEventsByUser = async (req, res) => {
   }
 };
 
-const UpdateTemplate = async (req, res) => {
-  const { id } = req.params;
-  const templates = req.body;
-
-  try {
-    const event = await Event.findByIdAndUpdate(id, templates, { new: true, runValidators: true });
-    if (!event) {
-      return res.status(404).send({ error: 'User not found' });
-    }
-    res.send(event);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
-
-
-
 const getEventById = async (req, res) => {
   const id = req.params.id;
 
@@ -139,16 +122,13 @@ const updateEvent = async (req, res) => {
   const updateField = req.body;
 
   try {
-    const updatedEvent = await Event.findOneAndUpdate(
-      { _id: id },
-      updateField,
-      {
-        new: true,
-      }
-    );
-    res.status(200).send(updatedEvent);
+    const event = await Event.findByIdAndUpdate(id, updateField, { new: true, runValidators: true });
+    if (!event) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    res.send(event);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(400).send(error);
   }
 };
 
@@ -353,6 +333,6 @@ const updateReviewerStatus = async (req, res) => {
   }
 };
 
-module.exports = { getEvents,getEventsByUser, addEvent, getEventById, deleteEvent, updateEvent, getAllReviewersInEvent , addEditor,addReviewer, getEventIdByName ,updateReviewerStatus ,UpdateTemplate};
+module.exports = { getEvents,getEventsByUser, addEvent, getEventById, deleteEvent, updateEvent, getAllReviewersInEvent , addEditor,addReviewer, getEventIdByName ,updateReviewerStatus };
 
 
