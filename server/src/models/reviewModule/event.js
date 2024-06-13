502328915ce95b331b1802b3421a7b527df31903
Luson045 <lusonbasumatary2@gmail.com>
2024-06-13 23:59:37 +0530

    1)merged the users and prm-users collection 2)added a new key(name) in the models/users.js schema from prm-users 3)made changes in models/event.js schema where the user key was having reference to 'prm-users'

--- server/src/models/reviewModule/event.js
+++ server/src/models/reviewModule/event.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = require("./user.js");
 
 const reviewerSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'PRS-User' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     status: { type: String, enum: ['Invited', 'Accepted', 'Pending', 'Not Accepted'], default: 'Pending' }
 });
 
@@ -28,7 +28,7 @@ const eventSchema = new mongoose.Schema({
         type: Date,
     },
     tracks:[{type: String}],
-    editor: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PRS-User' }],
+    editor: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
     reviewer: [reviewerSchema],
     paperSubmissionDate: {
         type: Date,
