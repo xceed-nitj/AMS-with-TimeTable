9f5352b417ac221e888fd3cccbd4f452103df6a2
SumitTeerthani <sumitteerthani2002@gmail.com>
2024-06-23 20:30:49 +0530

    #566 disabled when review gets submitted

--- server/src/models/reviewModule/review.js
+++ server/src/models/reviewModule/review.js
@@ -1,20 +1,26 @@
-const mongoose = require("mongoose");
-const User =require("./user");
-const Event =require("./event");
-const Paper=require("./paper");
+const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-    paperId: { type: String,  required:true },
-    eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-    reviewerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    reviewans: [{ type: String }],
-    commentsAuthor: { type: String },
-    commentsEditor: { type: String },
-    decision: { type: String, enum: ['Accepted', 'Rejected', 'Needs Revision'], required: true  },
-    updatedAt: { type: Date, default: Date.now },
-    createdAt: { type: Date, default: Date.now }
+  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+  paperId: { type: String, required: true },
+  reviewerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  reviewAnswers: [
+    {
+      questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'ReviewQuestion' },
+      answer: { type: mongoose.Schema.Types.Mixed } // Mixed type to store any kind of answer
+    }
+  ],
+  commentsAuthor: { type: String },
+  commentsEditor: { type: String },
+  decision: {
+    type: String,
+    enum: ['Accepted', 'Rejected', 'Need Revision'],
+    default: 'Need Revision'
+  },
+  updatedAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now }
 });
 
-const Review = mongoose.model("Review", reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
