54b666f6403d26db277a3bc844f55bca118239b9
SumitTeerthani <sumitteerthani2002@gmail.com>
2024-06-25 17:03:36 +0530

    added order field in defaultQuestion.js #648

--- server/src/models/reviewModule/defaultQuestion.js
+++ server/src/models/reviewModule/defaultQuestion.js
@@ -4,7 +4,8 @@ const defaultQuestionSchema = new mongoose.Schema({
     show: { type: Boolean, default: false },
     type: [{ type: String }],
     question: [{ type: String }],
-    options: [{ type: String }]
+    options: [{ type: String }],
+    order: [{type:Number}]
 },{timestamps: true});
 
 const DefaultQuestion = mongoose.model("PRS-DefaultQuestion", defaultQuestionSchema);
