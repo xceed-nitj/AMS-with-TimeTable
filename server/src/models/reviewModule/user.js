c63caff046b0393833f76f2bf6f162f1c06f57d6
SumitTeerthani <sumitteerthani2002@gmail.com>
2024-06-29 17:07:10 +0530

    #703 Main user in usermanagement will be used for reviewmodule user

--- server/src/models/reviewModule/user.js
+++ server/src/models/reviewModule/user.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
+const UserManagementSchema = require("../../models/usermanagement/user");
 
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    role:{
-        type:String,
-        enum: ['Author','Editor','Reviewer'],
-        required: true,
-    },
-    password: { type: String, required: true },
-    experience: {
-        Dept:{type: String},
-        Designation:{type: String},
-        College:{type: String},
-        Period:{type: Number},
-    },
-    profession: {type: String},
-    email: [{type: String, required: true}],
-    area:[{type: String}],
-});
+const User = mongoose.model("PRS-User", UserManagementSchema.schema);
 
-const User = mongoose.model("PRS-User", userSchema);
+// const userSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     role:{
+//         type:String,
+//         enum: ['Author','Editor','Reviewer'],
+//         required: true,
+//     },
+//     password: { type: String, required: true },
+//     experience: {
+//         Dept:{type: String},
+//         Designation:{type: String},
+//         College:{type: String},
+//         Period:{type: Number},
+//     },
+//     profession: {type: String},
+//     email: [{type: String, required: true}],
+//     area:[{type: String}],
+// });
+
+// const User = mongoose.model("PRS-User", userSchema);
 
 module.exports = User;
