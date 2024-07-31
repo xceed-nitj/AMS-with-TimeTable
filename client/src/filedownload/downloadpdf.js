3645796793b5ac9aba028ddcf3513a4c1345ffc1
i-ankit-here <closetooankit@gmail.com>
2024-07-31 19:50:12 +0530

    updated font error

--- client/src/filedownload/downloadpdf.js
+++ client/src/filedownload/downloadpdf.js
@@ -1,13 +1,14 @@
 import pdfMakeInitializer from './pdfMakeInitializer';
 // Import other dependencies
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as vfs_fonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = vfs_fonts.pdfMake.vfs;
 
 pdfMakeInitializer(); 
 
 import header from '../assets/header.png';
 import footer from '../assets/footer.png';
 import { CustomTh, CustomLink, CustomBlueButton } from '../styles/customStyles';
-
-//pdfMake.vfs = pdfFonts.vfs;
 
 
 // pdfMake.vfs = pdfFonts.pdfMake.vfs;
