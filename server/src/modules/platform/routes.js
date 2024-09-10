3d5aa35fdde3104b4bc58ccf1db749cea0b6188a
JAYANTJOSHI001 <jayantjoshi909@gmail.com>
2024-09-10 22:24:35 +0530

    rectified bug in route of platform
    Signed-off-by: JAYANTJOSHI001 <jayantjoshi909@gmail.com>

--- server/src/modules/platform/routes.js
+++ server/src/modules/platform/routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {addPlatform,getPlatform,updatePlatform,deletePlatform,getPlatformById,addService} = require('../platform/controller');
+const {addPlatform,getPlatform,updatePlatform,deletePlatform,getPlatformById} = require('../platform/controller');
 
 router.post("/add", addPlatform);
-router.post("/add/service", addService);
 router.get("/getplatform", getPlatform);
 router.get("/get/:id", getPlatformById);
 router.patch("/update/:id", updatePlatform);
