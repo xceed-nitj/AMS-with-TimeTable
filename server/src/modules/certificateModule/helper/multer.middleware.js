bebafeda09f1301d4e9a6d7dca0dcf3bedc6d41d
i-ankit-here <closetooankit@gmail.com>
2024-10-23 22:03:17 +0530

    updated file path

--- server/src/modules/certificateModule/helper/multer.middleware.js
+++ server/src/modules/certificateModule/helper/multer.middleware.js
@@ -1,8 +1,10 @@
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads/certificateModuleImages");
+    const p = path.join(__dirname, '../../../../uploads/certificateModuleImages');
+    cb(null, p);
   },
   filename: function (req, file, cb) {
     const userId = req?.user?.id;
