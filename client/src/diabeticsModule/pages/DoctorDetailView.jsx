0d1be61186b5c2ca29fa36884cedd29cbcc4f182
ggsingla <ggsingla777@gmail.com>
2025-03-19 16:25:44 +0530

    feat: add doctor and hospital detail view components

---
+++ client/src/diabeticsModule/pages/DoctorDetailView.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import DoctorDetailViewComponent from '../components/admin/DoctorDetailView';
+
+export default function DoctorDetailView() {
+  return <DoctorDetailViewComponent />;
+}
