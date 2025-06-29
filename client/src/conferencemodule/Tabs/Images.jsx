8aa6114a888e67666f602842a745bfe8bc474d73
Krishna27Singh <krishnasingh97857@gmail.com>
2025-06-29 17:21:53 +0530

    conference module images UI update

--- client/src/conferencemodule/Tabs/Images.jsx
+++ client/src/conferencemodule/Tabs/Images.jsx
@@ -1,294 +1,203 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import LoadingIcon from "../components/LoadingIcon";
 import getEnvironment from "../../getenvironment";
-import { Container } from "@chakra-ui/layout";
 import {
-    FormControl, FormErrorMessage, FormLabel, Center, Heading,
-    Input, Button, Select
-} from '@chakra-ui/react';
-import { CustomTh, CustomLink, CustomBlueButton } from '../utils/customStyles'
-import {
-    Table,
-    TableContainer,
-    Tbody,
-    Td,
-    Th,
-    Thead,
-    Tr,
-} from "@chakra-ui/table";
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Select,
+  Center,
+  Heading,
+  Box
+} from "@chakra-ui/react";
+
 const Images = () => {
-    const params = useParams();
-const IdConf = params.confid;
-    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
-    const [deleteItemId, setDeleteItemId] = useState(null);    const apiUrl = getEnvironment();
+  const params = useParams();
+  const IdConf = params.confid;
+  const apiUrl = getEnvironment();
 
-    const initialData = {
-        "confId": IdConf,
-        "name": "",
-        "imgLink": "",
-        "feature": true,
-        "sequence": "",
+  const initialData = {
+    confId: IdConf,
+    name: "",
+    imgLink: "",
+    feature: true,
+    sequence: "",
+  };
+
+  const [formData, setFormData] = useState(initialData);
+  const [editID, setEditID] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [deleteItemId, setDeleteItemId] = useState(null);
+
+  const { name, imgLink, sequence } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]:
+        name === "sequence" ? parseInt(value) :
+        name === "feature" ? value === "true" :
+        value
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (editID) {
+      axios
+        .put(`${apiUrl}/conferencemodule/images/${editID}`, formData, {
+          withCredentials: true,
+        })
+        .then(() => {
+          alert("Updated successfully");
+        })
+        .catch((err) => console.log(err));
+    } else {
+      axios
+        .post(`${apiUrl}/conferencemodule/images`, formData, {
+          withCredentials: true,
+        })
+        .then(() => {
+          alert("Added successfully");
+          setFormData(initialData);
+        })
+        .catch((err) => console.log(err));
     }
+  };
 
-    const [formData, setFormData] = useState(initialData);
+  const handleDelete = (id) => {
+    setDeleteItemId(id);
+    setShowDeleteConfirmation(true);
+  };
 
-    const [editID, setEditID] = useState("");
-    const [data, setData] = useState([]);
-    const [refresh, setRefresh] = useState(0);
-    const [loading, setLoading] = useState(false);
+  const confirmDelete = () => {
+    axios
+      .delete(`${apiUrl}/conferencemodule/images/${deleteItemId}`, {
+        withCredentials: true,
+      })
+      .then(() => {
+        alert("Deleted");
+        setFormData(initialData);
+        setEditID(null);
+        setShowDeleteConfirmation(false);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const { name, imgLink,sequence } = formData;
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        if (name === "sequence") {
-            setFormData({
-                ...formData,
-                [name]: parseInt(value),
-            });
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`${apiUrl}/conferencemodule/images/conference/${IdConf}`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        if (res.data.length > 0) {
+          const firstImage = res.data[0];
+          setFormData(firstImage);
+          setEditID(firstImage._id);
         }
-        else if (name === "feature") {
-            setFormData({
-                ...formData,
-                [name]: value === "true",
-            });
-        }
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [IdConf]);
 
-        else {
-            setFormData({
-                ...formData,
-                [name]: value,
-            });
-        }
-
-    };
-
-    const handleSubmit = (e) => {
-        // e.preventDefault();
-
-        axios.post(`${apiUrl}/conferencemodule/images`, formData, {
-            withCredentials: true
-
-        })
-            .then(res => {
-                setData([...data, res.data]);
-                setFormData(initialData);
-                setRefresh(refresh + 1);
-
-            })
-            .catch(err => {
-                console.log(formData)
-                console.log(err)
-            });
-    };
-
-    const handleUpdate = () => {
-        axios.put(`${apiUrl}/conferencemodule/images/${editID}`, formData, {
-            withCredentials: true
-
-        })
-            .then(res => {
-                setFormData(initialData);
-                setRefresh(refresh + 1);
-                setEditID(null)
-            })
-            .catch(err => console.log(err));
-    };
-
-    const handleDelete = (deleteID) => {
-        setDeleteItemId(deleteID);
-        setShowDeleteConfirmation(true);
-    };
-
-    const confirmDelete = () => {
-        axios.delete(`${apiUrl}/conferencemodule/images/${deleteItemId}`, {
-            withCredentials: true
-
-        })
-            .then(res => {
-                console.log('DELETED RECORD::::', res);
-                               setShowDeleteConfirmation(false);  
-                 setRefresh(refresh + 1);
-          
-                setFormData(initialData);
-            })
-            .catch(err => console.log(err));
-    };
-
-    const handleEdit = (editIDNotState) => {
-        window.scrollTo(0, 0);
-        axios.get(`${apiUrl}/conferencemodule/images/${editIDNotState}`, {
-            withCredentials: true
-
-        })
-            .then(res => {
-                setFormData(res.data);
-            })
-            .catch(err => {
-                console.log(data);
-                console.log(err);
-            });
-    };
-
-    useEffect(() => {
-        setLoading(true);
-        axios.get(`${apiUrl}/conferencemodule/images/conference/${IdConf}`, {
-            withCredentials: true
-
-        })
-            .then(res => {
-                setData(res.data);
-            })
-            .catch(err => console.log(err))
-            .finally(() => setLoading(false));
-    }, [refresh]);
-
+  if (loading) {
     return (
-        <main className='tw-py-10 lg:tw-pl-72 tw-min-h-screen'>
-    
-            <Container maxW='5xl'>
-                <Heading as="h1" size="xl" mt="6" mb="6">
-                    Add a New Image
-                </Heading>
-
-
-                <FormControl isRequired={true} mb='3' >
-                    <FormLabel >Name:</FormLabel>
-                    <Input
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={handleChange}
-                        placeholder="Name"
-                        mb='2.5'
-                    />
-                </FormControl>
-               
-                <FormControl isRequired={true} mb='3' >
-                    <FormLabel >Image Link :</FormLabel>
-                    <Input
-                        type="text"
-                        name="imgLink"
-                        value={imgLink}
-                        onChange={handleChange}
-                        placeholder="Image Link"
-                        mb='2.5'
-                    />
-                </FormControl>
-                
-
-                <FormControl isRequired={true}  >
-
-                    <FormLabel >Sequence :</FormLabel>
-                    <Input
-
-                        type="number"
-                        name="sequence"
-                        value={sequence}
-                        onChange={handleChange}
-                        placeholder="sequence"
-                        mb='2.5'
-                   />
-                   </FormControl>
-                <FormControl isRequired={true} mb='3' >
-                    <FormLabel >Feature:</FormLabel>
-                    <Select
-                        name="feature"
-                        value={formData.feature}
-                        onChange={handleChange}
-                    >
-                        <option value={true}>Yes</option>
-                        <option value={false}>No</option>
-                    </Select>
-                </FormControl>
-               
-
-                <Center>
-              
-                    <Button colorScheme="blue" type={editID ? "button" : "submit"} onClick={() => { editID ? handleUpdate() : handleSubmit() }}>
-                        {editID ? 'Update' : 'Add'}
-                    </Button>
-
-            </Center>
-                <Heading as="h1" size="xl" mt="6" mb="6">
-                    Existing Awards </Heading>
-                {!loading ? (
-
-                    <TableContainer>
-                        <Table
-                            variant='striped'
-                            size="md"
-                            mt="1"
-                        >
-                            <Thead>
-                                <Tr>
-                                    <CustomTh>Name</CustomTh>
-                                    <CustomTh>Link</CustomTh>
-                                    <CustomTh>Sequence</CustomTh>
-                                    <CustomTh>Feature</CustomTh>
-
-
-                                    <CustomTh position={'sticky'} right={'0'}>Action</CustomTh>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                {data.length > 0 ? (data.map((item) => (
-                                    <Tr key={item._id}>
-                                        <Td sx={{ maxWidth: '200px', whiteSpace: 'normal', wordWrap: 'break-word' }}>{item.name}</Td>
-                                        <Td sx={{ maxWidth: '200px', whiteSpace: 'normal', wordWrap: 'break-word' }}>{item.imgLink}</Td>
-                                        <Td sx={{ maxWidth: '100px', whiteSpace: 'normal', wordWrap: 'break-word' }}>{item.sequence}</Td>
-                                        <Td sx={{ maxWidth: '100px', whiteSpace: 'normal', wordWrap: 'break-word' }}>{item.feature?"Yes":"No"}</Td>
-
-                                        <Td position={'sticky'} right={'0'}><Center>
-                                            <Button colorScheme="red" onClick={() => handleDelete(item._id)}>Delete </Button>
-                                            <Button colorScheme="teal" onClick={() => {
-                                                handleEdit(item._id);
-                                                setEditID(item._id);
-                                            }}>Edit </Button>
-                                        </Center></Td>
-
-                                    </Tr>))) :
-                                    (
-                                        <Tr>
-                                            <Td colSpan="4" className="tw-p-1 tw-text-center">
-                                                <Center>No data available</Center></Td>
-                                        </Tr>
-                                    )
-                                }
-                            </Tbody>
-                        </Table>
-                    </TableContainer>
-                )
-
-                    : <LoadingIcon />
-                } </Container>
- 
-            {showDeleteConfirmation && (
-                <div className="tw-fixed tw-inset-0 tw-bg-black tw-bg-opacity-50 tw-flex tw-items-center tw-justify-center">
-                    <div className="tw-bg-white tw-rounded tw-p-8 tw-w-96">
-                        <p className="tw-text-lg tw-font-semibold tw-text-center tw-mb-4">
-                            Are you sure you want to delete?
-                        </p>
-                        <div className="tw-flex tw-justify-center">
-                            <Button
-                                colorScheme="red"
-                                onClick={confirmDelete}
-                                mr={4}
-                            >
-                                Yes, Delete
-                            </Button>
-                            <Button
-                                colorScheme="blue"
-                                onClick={() => setShowDeleteConfirmation(false)}
-                            >
-                                Cancel
-                            </Button>
-                        </div>
-                    </div>
-                </div>
-            )}        </main>
+      <main className="tw-py-10 tw-min-h-screen tw-flex tw-justify-center tw-items-center">
+        <LoadingIcon />
+      </main>
     );
+  }
+
+  return (
+    <main className="tw-py-10 tw-min-h-screen tw-flex tw-justify-center">
+      <Box className="tw-w-full tw-max-w-2xl tw-px-4">
+        <Container maxW="full">
+          <Center mb="6">
+            <Heading as="h1" size="xl" mt="6" mb="6" style={{
+                                color: "#14B8A6", 
+                                textDecoration: "underline"
+                            }}>
+              Add a New Image
+            </Heading>
+          </Center>
+
+          <FormControl isRequired mb="4">
+            <FormLabel>Name:</FormLabel>
+            <Input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+              placeholder="Name"
+            />
+          </FormControl>
+
+          <FormControl isRequired mb="4">
+            <FormLabel>Image Link:</FormLabel>
+            <Input
+              type="text"
+              name="imgLink"
+              value={imgLink}
+              onChange={handleChange}
+              placeholder="Image Link"
+            />
+          </FormControl>
+
+          <FormControl isRequired mb="4">
+            <FormLabel>Sequence:</FormLabel>
+            <Input
+              type="number"
+              name="sequence"
+              value={sequence}
+              onChange={handleChange}
+              placeholder="Sequence"
+            />
+          </FormControl>
+
+          <FormControl isRequired mb="6">
+            <FormLabel>Feature:</FormLabel>
+            <Select name="feature" value={formData.feature} onChange={handleChange}>
+              <option value={true}>Yes</option>
+              <option value={false}>No</option>
+            </Select>
+          </FormControl>
+
+          <Center>
+            <Button colorScheme="blue" onClick={handleSubmit}>
+              {editID ? "Update" : "Add"}
+            </Button>
+          </Center>
+        </Container>
+      </Box>
+
+      {showDeleteConfirmation && (
+        <div className="tw-fixed tw-inset-0 tw-bg-black tw-bg-opacity-50 tw-flex tw-items-center tw-justify-center">
+          <div className="tw-bg-white tw-rounded tw-p-8 tw-w-96">
+            <p className="tw-text-lg tw-font-semibold tw-text-center tw-mb-4">
+              Are you sure you want to delete?
+            </p>
+            <div className="tw-flex tw-justify-center">
+              <Button colorScheme="red" onClick={confirmDelete} mr={4}>
+                Yes, Delete
+              </Button>
+              <Button
+                colorScheme="blue"
+                onClick={() => setShowDeleteConfirmation(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Images;
